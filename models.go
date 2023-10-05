22e5e6f61fda12819c7a544994bbb56564574317
Ivan Solovyev <1707782+isoloviev@users.noreply.github.com>
2023-10-05 00:04:10 -0400

    PAS-1 Introduce simgo

--- models.go
+++ models.go
@@ -27,29 +27,28 @@ const (
 )
 
 type Report struct {
-	AltitudeAboveGround float64 `name:"PLANE ALT ABOVE GROUND" unit:"feet"`
-	PlaneAltitude       float64 `name:"PLANE ALTITUDE" unit:"Radians"`
-	Altitude            float64 `name:"INDICATED ALTITUDE" unit:"feet"`
-	RadioHeight         float64 `name:"RADIO HEIGHT" unit:"feet"`
-	Latitude            float64 `name:"PLANE LATITUDE" unit:"degrees"`
-	Longitude           float64 `name:"PLANE LONGITUDE" unit:"degrees"`
-	Heading             float64 `name:"PLANE HEADING DEGREES TRUE" unit:"degrees"`
-	HeadingMagnetic     float64 `name:"PLANE HEADING DEGREES MAGNETIC" unit:"degrees"`
-	Airspeed            float64 `name:"AIRSPEED INDICATED" unit:"knot"`
-	AirspeedTrue        float64 `name:"AIRSPEED TRUE" unit:"knot"`
-	AirspeedMach        float64 `name:"AIRSPEED MACH" unit:"Mach"`
-	VerticalSpeed       float64 `name:"VERTICAL SPEED" unit:"ft/min"`
-	Flaps               float64 `name:"TRAILING EDGE FLAPS LEFT ANGLE" unit:"degrees"`
-	Trim                float64 `name:"ELEVATOR TRIM PCT" unit:"percent"`
-	RudderTrim          float64 `name:"RUDDER TRIM PCT" unit:"percent"`
-	WindDirection       float64 `name:"AMBIENT WIND DIRECTION" unit:"degrees"`
-	WindVelocity        float64 `name:"AMBIENT WIND VELOCITY" unit:"knots"`
-	SurfaceType         float64 `name:"SURFACE TYPE" unit:"Enum"`
-	OnAnyRunway         float64 `name:"ON ANY RUNWAY" unit:"Bool"`
-	GroundVelocity      float64 `name:"GROUND VELOCITY" unit:"knots"`
-	TouchdownLat        float64 `name:"PLANE TOUCHDOWN LATITUDE" unit:"radians"`
-	TouchdownLon        float64 `name:"PLANE TOUCHDOWN LONGITUDE" unit:"radians"`
-	//TouchdownVelocity     float64 `name:"PLANE TOUCHDOWN NORMAL VELOCITY" unit:"Feetpersecond"`
+	AltitudeAboveGround   float64 `name:"PLANE ALT ABOVE GROUND" unit:"feet"`
+	PlaneAltitude         float64 `name:"PLANE ALTITUDE" unit:"Radians"`
+	Altitude              float64 `name:"INDICATED ALTITUDE" unit:"feet"`
+	RadioHeight           float64 `name:"RADIO HEIGHT" unit:"feet"`
+	Latitude              float64 `name:"PLANE LATITUDE" unit:"degrees"`
+	Longitude             float64 `name:"PLANE LONGITUDE" unit:"degrees"`
+	Heading               float64 `name:"PLANE HEADING DEGREES TRUE" unit:"degrees"`
+	HeadingMagnetic       float64 `name:"PLANE HEADING DEGREES MAGNETIC" unit:"degrees"`
+	Airspeed              float64 `name:"AIRSPEED INDICATED" unit:"knot"`
+	AirspeedTrue          float64 `name:"AIRSPEED TRUE" unit:"knot"`
+	AirspeedMach          float64 `name:"AIRSPEED MACH" unit:"Mach"`
+	VerticalSpeed         float64 `name:"VERTICAL SPEED" unit:"ft/min"`
+	Flaps                 float64 `name:"TRAILING EDGE FLAPS LEFT ANGLE" unit:"degrees"`
+	Trim                  float64 `name:"ELEVATOR TRIM PCT" unit:"percent"`
+	RudderTrim            float64 `name:"RUDDER TRIM PCT" unit:"percent"`
+	WindDirection         float64 `name:"AMBIENT WIND DIRECTION" unit:"degrees"`
+	WindVelocity          float64 `name:"AMBIENT WIND VELOCITY" unit:"knots"`
+	SurfaceType           float64 `name:"SURFACE TYPE" unit:"Enum"`
+	OnAnyRunway           float64 `name:"ON ANY RUNWAY" unit:"Bool"`
+	GroundVelocity        float64 `name:"GROUND VELOCITY" unit:"knots"`
+	TouchdownLat          float64 `name:"PLANE TOUCHDOWN LATITUDE" unit:"radians"`
+	TouchdownLon          float64 `name:"PLANE TOUCHDOWN LONGITUDE" unit:"radians"`
 	TouchdownBank         float64 `name:"PLANE TOUCHDOWN BANK DEGREES" unit:"degrees"`
 	TouchdownPitch        float64 `name:"PLANE TOUCHDOWN PITCH DEGREES" unit:"degrees"`
 	VelocityBodyY         float64 `name:"VELOCITY BODY Y" unit:"Feetpersecond"`
@@ -76,7 +75,6 @@ type Report struct {
 	PushbackAttached      float64 `name:"PUSHBACK ATTACHED" unit:"Bool"`
 	PushbackAvailable     float64 `name:"PUSHBACK AVAILABLE" unit:"Bool"`
 	BrakeParkingIndicator float64 `name:"BRAKE PARKING INDICATOR" unit:"Bool"`
-	BrakeParkingPosition  float64 `name:"BRAKE PARKING POSITION" unit:"position 32k"`
 	GearIsOnGround        float64 `name:"GEAR IS ON GROUND" unit:"Bool"`
 	GForce                float64 `name:"G FORCE" unit:"GForce"`
 	GForceMin             float64 `name:"MIN G FORCE" unit:"GForce"`
